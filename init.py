92f2466f60f1e216c82e3a311c7b5934ff2395b9
itsriley <160186931+itsriley@users.noreply.github.com>
2024-06-09 23:38:58 -0700

    Add files via upload

---
+++ init.py
@@ -0,0 +1,24 @@
+# -----------------------------------------------------------
+#  init.py
+#  Version: 1.0.1
+#  Last Updated: May 25, 2024
+# -----------------------------------------------------------
+
+
+print("""
+==================== Pipeline Messages =======================
+
+'Welcome!'
+
+init path:""")
+print(__file__)
+
+
+sys.path.append("F:/Alexander_Richter_Python_for_DCC/Final_Project/")
+
+
+# ----- DEFINE CUSTOM FOLDER STRUCTURE ----------------------
+# nuke.pluginAddPath('./gizmos')
+# nuke.pluginAddPath('./python')
+
+
